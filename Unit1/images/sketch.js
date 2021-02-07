d65ea8032e18d3f814481386126dc96479c1e5a9
sdfewke <64711969+sdfewke@users.noreply.github.com>
2021-02-06 23:29:02 -0600

    Finished Images Project
    Added images to js

--- Unit1/images/sketch.js
+++ Unit1/images/sketch.js
@@ -1,7 +1,20 @@
+let p1, p2, p3;
+
 function setup() {
-  createCanvas(500, 500);
+  createCanvas(800, 800);
+
+  p1 = loadImage("assets/hockey.jpg");
+  p2 = loadImage("assets/juice.jpg");
+  p3 = loadImage("assets/pc.jpg");
+
+  imageMode(CENTER);
 }
 
 function draw() {
+background('white');
+
+image(p1, width/2, height/2, 200, 200);
+image(p2, width/2, height/2 - 200, 200, 200);
+image(p3, width/2, height/2 + 200, 200, 200);
 
 }
