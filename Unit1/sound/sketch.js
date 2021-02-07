d313011c093336c348fd59cb2c23d41ccc887987
sdfewke <64711969+sdfewke@users.noreply.github.com>
2021-02-06 22:28:19 -0600

    Sound File Created
    added project "Sound"

---
+++ Unit1/sound/sketch.js
@@ -0,0 +1,25 @@
+let song1;
+
+function preload() {
+  song1 = loadSound("assets/jazz.mp3");
+}
+
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+background(100);
+}
+
+function mouseReleased() {
+  if (song1.isPlaying()) {
+    song1.pause();
+  } else {
+    song1.loop();
+  }
+}
+
+function touchStarted() {
+  getAudioContext().resume();
+}
