13374eb8b68e6cc1602ea8b45d566f42f757802c
sdfewke <64711969+sdfewke@users.noreply.github.com>
2021-03-28 17:56:28 -0500

    paticle systems

---
+++ Unit3/part1/sketch.js
@@ -0,0 +1,52 @@
+let cars = [];
+
+function setup() {
+  createCanvas(500, 500);
+
+  // for (let i = 0; i < 20; i++) {
+  //   cars.push(new Car());
+  // }
+noStroke();
+}
+
+function draw() {
+  background('grey');
+  cars.push(new Car());
+
+
+  for (let i = 0; i < cars.length; i++) {
+    cars[i].display();
+    cars[i].move();
+    if (cars[i].a <= 0) {
+      cars.splice(i, 1);
+    }
+  }
+}
+
+
+class Car {
+
+  constructor() {
+    //attributes
+    this.pos = createVector(width / 2, height - 80);
+    this.vel = createVector(random(-.8, .8), random(-10, -5));
+    this.r = 255 ; //random(255);
+    this.g = 255 ; //random(255);
+    this.b = 255 ; //random(255);
+    this.a = random(200, 255);
+  }
+
+  //methods
+  display() {
+    fill(this.r, this.g, this.b, this.a);
+    ellipse(this.pos.x, this.pos.y, 20);
+  }
+
+  move() {
+    this.pos.add(this.vel);
+    this.a = this.a - 5;
+  }
+
+
+
+}
